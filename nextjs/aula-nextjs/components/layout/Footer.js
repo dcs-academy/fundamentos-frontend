aa62e77fa29876c0214bfb8fa21109a00b4f7e56
Daniel Castro <danielcastro.rs@gmail.com>
2022-11-09 22:11:53 -0300

    feat: source nextjs lesson 04

---
+++ nextjs/aula-nextjs/components/layout/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <a href="https://danieldcs.com" target="_blank" rel="noopener noreferrer">
+        Powered by @odanieldcs
+      </a>
+    </footer>
+  );
+}
---
+++ nextjs/aula-nextjs/components/layout/Header.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+const paths = [
+  { path: "/", label: "Página inicial" },
+  { path: "/account", label: "Minha Conta" },
+  { path: "/category", label: "Categorias" },
+  { path: "/contact", label: "Contato" },
+];
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        {paths.map((path) => (
+          <Link href={path.path} key={path.path}>
+            {path.label}
+          </Link>
+        ))}
+      </nav>
+    </header>
+  );
+}
