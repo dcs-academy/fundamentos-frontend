b6a2ad6288ba2922e3f27c6aeb1cdc9b71a674b8
Daniel Castro <danielcastro.rs@gmail.com>
2023-08-30 12:17:12 -0300

    feat: source nextjs lesson images

--- nextjs/aula-nextjs/pages/category/informatica.js
+++ nextjs/aula-nextjs/pages/category/informatica.js
@@ -1,6 +1,32 @@
+import Image from "next/image";
+import informatica from "../../public/images/informatica.jpg";
+
 export default function PageCategoryInformatica() {
   return (
     <div>
+      <Image
+        alt="Informática"
+        src={informatica}
+        quality={80}
+        placeholder="blur"
+        blurDataURL=""
+        sizes="100vw"
+        style={{
+          maxWidth: "100%",
+          height: "auto",
+        }}
+      />
+      {/* <Image
+        alt="Livros"
+        src="/images/livros.jpg"
+        width={1080}
+        height={280}
+        sizes="100vw"
+        style={{
+          maxWidth: "100%",
+          height: "auto",
+        }}
+      /> */}
       <h1>Categoria Informática</h1>
       <p>Texto de exemplo.</p>
     </div>
