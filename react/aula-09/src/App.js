54d93388c80463f510c2698b8aea032614d324bd
Daniel Castro <danielcastro.rs@gmail.com>
2023-09-02 18:24:08 -0300

    feat: source code of react lesson 09

---
+++ react/aula-09/src/App.js
@@ -0,0 +1,13 @@
+// import ClassComponent from "./lessons/class-component";
+import SampleUseState from "./lessons/hooks/use-state";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <SampleUseState />
+    </div>
+  );
+}
+
+export default App;
