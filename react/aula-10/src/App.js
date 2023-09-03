fc0e88501e9527b1364dd756018f872a54bd5295
Daniel Castro <danielcastro.rs@gmail.com>
2023-09-03 17:05:14 -0300

    feat: source code of react lesson 10

---
+++ react/aula-10/src/App.js
@@ -0,0 +1,15 @@
+// import ClassComponent from "./lessons/class-component";
+// import SampleUseState from "./lessons/hooks/useState";
+import SampleUseEffect from "./lessons/hooks/useEffect";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <SampleUseEffect />
+    </div>
+  );
+}
+
+export default App;
